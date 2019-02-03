a863f22f9a9459e0787fdf9ada86e0dca0107a52
hosein74 <hosein.z.74@gmail.com>
2019-02-03 08:55:28 +0330

    finished

--- public/js/algorithm.js
+++ public/js/algorithm.js
@@ -39,75 +39,98 @@ switch (path[1]) {
         switch (path[3]) {
             case 'animation':
                 $('#mySecondHeader').text('بخش نمایش الگوریتم')
+                $('#mySecondHeader1').text('بخش نمایش الگوریتم')
                 break;
             case 'analyze':
                 $('#mySecondHeader').text('بخش تحلیل الگوریتم')
+                $('#mySecondHeader1').text('بخش تحلیل الگوریتم')
                 break;
             case 'help-file':
                 $('#mySecondHeader').text('بخش راهنمای فایل')
+                $('#mySecondHeader1').text('بخش راهنمای فایل')
                 break;
             case 'description':
                 $('#mySecondHeader').text('بخش توضیحات الگوریتم')
+                $('#mySecondHeader1').text('بخش توضیحات الگوریتم')
                 break;
         }
 
         switch (path[2]) {
             case 'bubble-sort':
                 $('#myFirstHeader').text('(bubble sort) مرتب سازی حبابی')
+                $('#myFirstHeader1').text('(bubble sort) مرتب سازی حبابی')
                 break;
             case 'selection-sort':
                 $('#myFirstHeader').text('(selection sort) مرتب سازی انتخابی')
+                $('#myFirstHeader1').text('(selection sort) مرتب سازی انتخابی')
                 break;
             case 'insertion-sort':
                 $('#myFirstHeader').text('(insertion sort) مرتب سازی درجی')
+                $('#myFirstHeader1').text('(insertion sort) مرتب سازی درجی')
                 break;
             case 'quick-sort':
                 $('#myFirstHeader').text('(quick sort) مرتب سازی سریع')
+                $('#myFirstHeader1').text('(quick sort) مرتب سازی سریع')
                 break;
             case 'merge-sort':
                 $('#myFirstHeader').text('(merge sort) مرتب سازی ادغامی')
+                $('#myFirstHeader1').text('(merge sort) مرتب سازی ادغامی')
                 break;
             case 'heap-sort':
                 $('#myFirstHeader').text('(heap sort) مرتب سازی هیپ')
+                $('#myFirstHeader1').text('(heap sort) مرتب سازی هیپ')
                 break;
             case 'simple-matrix-multiple':
                 $('#myFirstHeader').text('(simple matrix multiple) ضرب معمولی ماتریس')
+                $('#myFirstHeader1').text('(simple matrix multiple) ضرب معمولی ماتریس')
                 break;
             case 'strassen--matrix-multiple':
                 $('#myFirstHeader').text('(strassen matrix multiple) ضرب استراسن ماتریس')
+                $('#myFirstHeader1').text('(strassen matrix multiple) ضرب استراسن ماتریس')
                 break;
             case 'simple-multiple':
                 $('#myFirstHeader').text('(simple multiple) ضرب معمولی اعداد')
+                $('#myFirstHeader1').text('(simple multiple) ضرب معمولی اعداد')
                 break;
             case 'karatsuba-multiple':
                 $('#myFirstHeader').text('(karatsuba-multiple)  ضرب کاراتسوبا')
+                $('#myFirstHeader1').text('(karatsuba-multiple)  ضرب کاراتسوبا')
                 break;
             case 'activity-selection':
                 $('#myFirstHeader').text('(activity selection) انتخاب فعالیت ها')
+                $('#myFirstHeader1').text('(activity selection) انتخاب فعالیت ها')
                 break;
             case 'maximum-sub-array':
                 $('#myFirstHeader').text('(maximum sub array) یافتن زیر آرایه بیشینه')
+                $('#myFirstHeader1').text('(maximum sub array) یافتن زیر آرایه بیشینه')
                 break;
             case 'huffman':
                 $('#myFirstHeader').text('(huffman) کد هافمن')
+                $('#myFirstHeader1').text('(huffman) کد هافمن')
                 break;
             case 'job-scheduling':
                 $('#myFirstHeader').text('(job scheduling) زمان بندی کار ها')
+                $('#myFirstHeader1').text('(job scheduling) زمان بندی کار ها')
                 break;
             case 'dfs':
                 $('#myFirstHeader').text('(dfs) پیمایش اول عمق')
+                $('#myFirstHeader1').text('(dfs) پیمایش اول عمق')
                 break;
             case 'bfs':
                 $('#myFirstHeader').text('(bfs) پیمایش اول سطح')
+                $('#myFirstHeader1').text('(bfs) پیمایش اول سطح')
                 break;
             case 'prim':
                 $('#myFirstHeader').text('(prim) پریم')
+                $('#myFirstHeader1').text('(prim) پریم')
                 break;
             case 'kruskals':
                 $('#myFirstHeader').text('(kruskals) کراسکال')
+                $('#myFirstHeader1').text('(kruskals) کراسکال')
                 break;
             case 'dijkstras':
                 $('#myFirstHeader').text('(dijkstras) دایجسترا')
+                $('#myFirstHeader1').text('(dijkstras) دایجسترا')
                 break;
         }
         break;
@@ -115,6 +138,7 @@ switch (path[1]) {
         switch (path[2]) {
             case 'sort':
                 $('#myFirstHeader').text('(comparing sort algorithms) مقایسه الگوریتم های مرتب سازی');
+                $('#myFirstHeader1').text('(comparing sort algorithms) مقایسه الگوریتم های مرتب سازی');
                 setAlogs([
                     {id:'bubble-sort',name:'حبابی'},
                     {id:'selection-sort',name:'انتخابی'},
@@ -126,6 +150,7 @@ switch (path[1]) {
                 break;
             case 'matrix-multiple':
                 $('#myFirstHeader').text('(comparing multiple-matrix algorithms) مقایسه الگوریتم های ضرب ماتریس')
+                $('#myFirstHeader1').text('(comparing multiple-matrix algorithms) مقایسه الگوریتم های ضرب ماتریس')
                 setAlogs([
                     {id:'simple-matrix-multiple',name:'معمولی'},
                     {id:'strassen--matrix-multiple',name:'استراسن'}
@@ -133,6 +158,7 @@ switch (path[1]) {
                 break;
             case 'multiple':
                 $('#myFirstHeader').text('(comparing multiple algorithms) مقایسه الگوریتم های ضرب معمولی')
+                $('#myFirstHeader1').text('(comparing multiple algorithms) مقایسه الگوریتم های ضرب معمولی')
                 setAlogs([
                     {id:'simple-multiple',name:'معمولی'},
                     {id:'karatsuba-multiple',name:'کاراتسوبا'}
@@ -140,6 +166,7 @@ switch (path[1]) {
                 break;
             case 'search':
                 $('#myFirstHeader').text('(comparing graph search algorithms) مقایسه الگوریتم های پیمایش گراف')
+                $('#myFirstHeader1').text('(comparing graph search algorithms) مقایسه الگوریتم های پیمایش گراف')
                 setAlogs([
                     {id:'dfs',name:'اول عمق'},
                     {id:'bfs',name:'اول سطح'}
@@ -147,6 +174,7 @@ switch (path[1]) {
                 break;
             case 'mst':
                 $('#myFirstHeader').text('(comparing minimum spanning tree algorithms) مقایسه الگوریتم های درخت پوشای مینیمال')
+                $('#myFirstHeader1').text('(comparing minimum spanning tree algorithms) مقایسه الگوریتم های درخت پوشای مینیمال')
                 setAlogs([
                     {id:'prim',name:'پریم'},
                     {id:'kruskals',name:'کراسکال'}
